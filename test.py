e193101f55138cb6f831ffe4afb7fe35af22ce3b
Sam-Fitzgerald <slf62@cam.ac.uk>
2024-05-10 15:05:36 +0100

    ABC

---
+++ test.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+mass_1 = 1
+mass_2 = 0.45
+m_per_tube = 0.45/N
+v_tube= m_per_tube/(rho_1*A_0)
+T_1 = 60
+T_2 = 20
+h_i = 386
+A_i =
+r_0 = 0.008
+r_i = 0.006
+k =
+L = 0.35
+A_0 = np.pi*r_0**2
+D_Noz = 0.02
+A_Noz = D_Noz**2*np.pi/4
+N = 10
+h_0 = 0.632
+mu = 6.51e-4
+H = 1/(1/h_i +A_i*np.ln(r_0/r_i)/(np.pi*2*k*L) +A_i/(A_0*h_0))
+Pr = 4.31
+Nu_i =
+c =
+Nu_0 = 
+rho_1 = 990.1
+A_sh =
+D_sh = 0.064
+Y = 0.014
+d_0 = 0.008
+Nb = 9
+B = L/(Nb+1)
+Cp = 4179
+delta_p_ends =
+Re_tube = rho_1*v_tube*
+v_noz2 = mass_2/(rho_1*A_Noz)
+delta_p_tube = 
+K_c
+K_e
+Q
